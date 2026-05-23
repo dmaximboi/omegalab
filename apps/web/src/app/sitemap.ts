fe784dc319287b7ca338d76c68617fc4621fded0
Uhhghhujbh <adewuyiayuba@gmail.com>
2026-05-23 05:39:30 +0000

    fix: add type annotation to filter callback in sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -57,7 +57,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     });
 
     productPages = products
-      .filter((p) => p.slug)
+      .filter((p: typeof products[number]) => p.slug)
       .map((product: typeof products[number]) => ({
         url: `${BASE_URL}/catalogue/${product.slug}`,
         lastModified: product.updatedAt,
